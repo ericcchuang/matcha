ef7ce648b582b9a60b4400d1606a356d4d7fb78b
ericcchuang <eyh3@njit.edu>
2025-11-07 21:13:10 -0500

    setting up fastapi

--- api/index.py
+++ api/index.py
@@ -3,18 +3,23 @@ import asyncio
 from dotenv import load_dotenv
 from dedalus_labs import AsyncDedalus, DedalusRunner
 from dedalus_labs.utils.streaming import stream_async
+from fastapi import FastAPI
+app = FastAPI()
 
 load_dotenv()
 
-async def main():
+@app.get("/")
+def read_root(input: str):
+    return {"response": asyncio.run(main(input))}
+
+async def main(input):
     client = AsyncDedalus()
     runner = DedalusRunner(client)
     response = await runner.run(
-        input="How do I create the next big thing?",
+        input=input,
         model="openai/gpt-4.1"
     )
+    return(response.final_output)
 
-    print(response.final_output)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+'''if __name__ == "__main__":
+    asyncio.run(main())'''
