eced62cc0789ab2f42934e80aa5087d3a70c1401
Logan Kriebel <logan.kriebel@gmail.com>
2025-11-08 17:34:38 -0500

    fixes 2

--- game/src/pages/Gacha.js
+++ game/src/pages/Gacha.js
@@ -18,43 +18,43 @@ function Gacha() {
     console.log(pulledCard);
     switch (pulledCard) {
       case 0:
-        setCardImageURL("../assets/cards/common/zero.png");
+        setCardImageURL("/assets/cards/common/zero.png");
         break;
       case 1:
-        setCardImageURL("../assets/cards/common/one.png");
+        setCardImageURL("/assets/cards/common/one.png");
         break;
       case 2:
-        setCardImageURL("../assets/cards/common/two.png");
+        setCardImageURL("/assets/cards/common/two.png");
         break;
       case 3:
-        setCardImageURL("../assets/cards/common/three.png");
+        setCardImageURL("/assets/cards/common/three.png");
         break;
       case 4:
-        setCardImageURL("../assets/cards/common/four.png");
+        setCardImageURL("/assets/cards/common/four.png");
         break;
       case 5:
-        setCardImageURL("../assets/cards/common/five.png");
+        setCardImageURL("/assets/cards/common/five.png");
         break;
       case 6:
-        setCardImageURL("../assets/cards/common/six.png");
+        setCardImageURL("/assets/cards/common/six.png");
         break;
       case 7:
-        setCardImageURL("../assets/cards/common/seven.png");
+        setCardImageURL("/assets/cards/common/seven.png");
         break;
       case 8:
-        setCardImageURL("../assets/cards/common/eight.png");
+        setCardImageURL("/assets/cards/common/eight.png");
         break;
       case 9:
-        setCardImageURL("../assets/cards/common/nine.png");
+        setCardImageURL("/assets/cards/common/nine.png");
         break;
       case 10:
-        setCardImageURL("../assets/cards/uncommon/plus.png");
+        setCardImageURL("/assets/cards/uncommon/plus.png");
         break;
       case 11:
-        setCardImageURL("../assets/cards/uncommon/minus.png");
+        setCardImageURL("/assets/cards/uncommon/minus.png");
         break;
       default:
-        setCardImageURL("../assets/icons/math.png");
+        setCardImageURL("/assets/icons/math.png");
     }
     // we don't have to use the cards setter because state will be updated by the currency setter
     if (pulledCard in cards) {
