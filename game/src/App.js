1da7d301e8a37c9bc0befd9277876f2da9686116
Pei <caabygail@gmail.com>
2025-11-08 11:20:51 -0500

    Started front-end design via CSS styles

--- game/src/App.js
+++ game/src/App.js
@@ -10,6 +10,19 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
+      <div className="App-header">
+        <div className="App-header-logo">
+          MATCHA
+          <img src={"../assets/matcha-currency.PNG"} />
+        </div>
+        <div className="App-header-stat">
+          <img src={"../assets/matcha-currency.PNG"} />
+          Currency
+        </div>
+        <div className="App-header-stat">
+          Meow
+        </div>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
